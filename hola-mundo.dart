e4a151d1ca71360163a7c4185a0a93fb43e4f49a
Aaron Rosa <sr.aaronrock@gmail.com>
2020-05-04 20:57:10 +0200

    Initial commit

---
+++ hola-mundo.dart
@@ -0,0 +1,5 @@
+void main() {
+
+  var nombre = 'Aaron';
+  print('Hola ' + nombre);
+}
---
+++ tipos-datos.dart
@@ -0,0 +1,16 @@
+void main() {
+
+  //Numeros Enteros
+  int empleados = 10;
+  //Numeros decimales
+  double pi = 3.1416;
+  double numero = 1.0;
+
+  print( '$empleados - $pi - $numero' );
+
+  //String
+  String nombre = 'Aaron';
+  print( 'Mi nombre es: '+nombre );
+  print( 'La primera letra de mi nombre es: '+nombre[0]); //Sacar la primera letra de mi nombre
+  print( 'La ultima letra de mi nombre es: '+nombre[ nombre.length - 1 ]); //Sacar la ultima letra
+}
